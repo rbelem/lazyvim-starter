55b1d3bd3b91117bcf0da861bcf09d440888d404
Rodrigo Belem <rodrigo.belem@gmail.com>
2025-02-13 01:44:41 -0400

    Initial configuration for the CodeCompanion plugin

---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    cmd = { "CodeCompanion", "CodeCompanionActions", "CodeCompanionChat" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      strategies = {
+        chat = {
+          adapter = "gemini",
+        },
+        inline = {
+          adapter = "gemini",
+        },
+      },
+      adapters = {
+        gemini = function()
+          return require("codecompanion.adapters").extend("gemini", {
+            env = {
+              api_key = os.getenv("GEMINI_API_KEY"),
+            },
+          })
+        end,
+      },
+    },
+  },
+}
