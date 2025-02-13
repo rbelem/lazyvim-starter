abb2debb37a4aa21bc42d65b611808c7c9bbdb34
Rodrigo Belem <rodrigo.belem@gmail.com>
2025-02-13 01:38:40 -0400

    Use kanagawa colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "rebelot/kanagawa.nvim",
+  },
+
+  {
+    "folke/tokyonight.nvim",
+    enabled = false,
+  },
+
+  {
+    "catppuccin/nvim",
+    enabled = false,
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = { colorscheme = "kanagawa" },
+  },
+}
