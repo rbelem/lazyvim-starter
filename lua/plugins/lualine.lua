6be5e9e896513991cbc99aaa70dcc5411dc39c9f
Rodrigo Belem <rodrigo.belem@gmail.com>
2025-02-13 13:17:17 -0400

    Customize lualine separatoris

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        theme = "auto",
+        component_separators = "|",
+        section_separators = "",
+      },
+    },
+  },
+}
