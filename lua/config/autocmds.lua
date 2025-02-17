a6e2794c1a7a66152eb4186d0d5c600257e14693
Rodrigo Belem <rodrigo.belem@gmail.com>
2025-02-17 00:08:06 -0400

    Save last cursor position on opened files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local last_position = vim.api.nvim_create_augroup("last_position", { clear = true })
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+  group = last_position,
+  desc = "Open the file in the same position it was last time",
+  pattern = "*",
+  command = 'silent! normal! g`"zv',
+})
