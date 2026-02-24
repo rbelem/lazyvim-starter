e27662fcb514b800c38a4478a4330573f42d7ab5
Rodrigo Belem <rodrigo.belem@gmail.com>
2026-02-24 14:52:04 -0400

    feat(keymaps): add terminal navigation and utility mappings
    Adds keybindings for opening the terminal, escaping terminal mode, and
    navigating windows while in terminal mode.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<C-t>", ":OpenTerm<cr>", { desc = "Open terminal" })
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Enter Normal Mode" })
+vim.keymap.set("t", "<C-w>", "<C-\\><C-n><C-w>", { desc = "Terminal window commands" })
+vim.keymap.set("t", "<C-h>", "<C-\\><C-n><C-w>h", { desc = "Go to left window" })
+vim.keymap.set("t", "<C-j>", "<C-\\><C-n><C-w>j", { desc = "Go to lower window" })
+vim.keymap.set("t", "<C-k>", "<C-\\><C-n><C-w>k", { desc = "Go to upper window" })
+vim.keymap.set("t", "<C-l>", "<C-\\><C-n><C-w>l", { desc = "Go to right window" })
